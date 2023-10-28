396ff1739809aaea1fd995743a86716cde7e6ba5
nacho503 <Your GitLab email address>
2023-10-27 22:39:10 -0300

    fix: typo in config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const PORT = process.env.PORT || 3000;
 
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.PASSWORD || 'tapi1740';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'tapi1740';
 export const DB_NAME = process.env.DB_NAME || 'usersdb';
 export const DB_PORT = process.env.DB_PORT || 3306;
